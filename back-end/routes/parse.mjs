87eb2b33fb6a4c2c67d40691e334380b94a6b2bd
shhwip <shhhwip@gmail.com>
2023-09-28 21:09:17 -0600

    added wikitext parsing

---
+++ back-end/routes/parse.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import Promise from "prfun";
+import fs from "fs";
+import Parsoid from "parsoid-jsapi";
+
+const router = express.Router();
+
+router.get("/parse", async (req, res) => {
+    // get the wikitext
+    // we will replace this with a call to the database
+    var lung = fs.readFileSync('lung.xml', 'utf8');
+    // console.log(lung.length)
+
+    // parse the wikitext into a parsoid document
+	var pdoc = await Parsoid.parse(lung, { pdoc: true });
+    // console.log("pdoc");
+    // console.log(pdoc);
+
+    // serialize the document so we can send it to the client
+    var record = await pdoc.document.serialize();
+    // console.log("record")
+	// console.log(record);
+    
+    // send the serialized document to the client
+  res.status(200).send(record);
+});
+
+
+export default router;
