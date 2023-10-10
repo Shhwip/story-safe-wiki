f87b69c18fa30b25b566f698d5ce501ff1abede3
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-10-10 01:01:05 -0600

    Search Display Works

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -22,40 +22,32 @@ function handleHeaderSearchClick() {
     setIsPopupActive(true);
 }
 
-function handleClickOutside(event) {
-    // for clicking outside of search bar
-    if (selectRef.current && !selectRef.current.contains(event.target)) {
-        setIsPopupActive(false);
-    }
+function handleCloseSearch() {
+    setIsPopupActive(false);
 }
 
-useEffect(() => {
-    window.addEventListener('click', handleClickOutside);
-
-    return () => {
-        window.removeEventListener('click', handleClickOutside);
-    };
-}, []);
 
     return (
-        <header className="app-header">
-            <img className="logo-header" src={logoHeader} alt="Combined Horizontal Logo for Story Safe"/>
-            <nav className="navigation">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"
-                     className={`search-nav ${isPopupActive ? 'active' : ''}`}
-                     ref={selectRef}
-                     onClick={handleHeaderSearchClick}>
-                    <path d="M256 64C150.13 64 64 150.13 64 256s86.13 192 192 192 192-86.13 192-192S361.87 64 256 64zm91.31 283.31a16 16 0 01-22.62 0l-42.84-42.83a88.08 88.08 0 1122.63-22.63l42.83 42.84a16 16 0 010 22.62z" />
-                    <circle cx="232" cy="232" r="56"/>
-                </svg>
-                <button className="login" onClick={handleHeaderLoginClick}>Sign In</button>
-                <button className="register" onClick={handleHeaderRegisterClick}>
-                    <img className="user-nav" src={user} alt="User Icon Circle Outline"></img>
-                    Register
-                </button>
-            </nav>
-            {isPopupActive && <Search />}
-        </header>
+        <div>
+            <header className="app-header">
+                <img className="logo-header" src={logoHeader} alt="Combined Horizontal Logo for Story Safe"/>
+                <nav className="navigation">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"
+                         className={`search-nav ${isPopupActive ? 'active' : ''}`}
+                         ref={selectRef}
+                         onClick={handleHeaderSearchClick}>
+                        <path d="M256 64C150.13 64 64 150.13 64 256s86.13 192 192 192 192-86.13 192-192S361.87 64 256 64zm91.31 283.31a16 16 0 01-22.62 0l-42.84-42.83a88.08 88.08 0 1122.63-22.63l42.83 42.84a16 16 0 010 22.62z" />
+                        <circle cx="232" cy="232" r="56"/>
+                    </svg>
+                    <button className="login" onClick={handleHeaderLoginClick}>Sign In</button>
+                    <button className="register" onClick={handleHeaderRegisterClick}>
+                        <img className="user-nav" src={user} alt="User Icon Circle Outline"></img>
+                        Register
+                    </button>
+                </nav>
+            </header>
+            {isPopupActive && <Search onCloseSearch={handleCloseSearch}/>}
+        </div>
     );
 }
 
