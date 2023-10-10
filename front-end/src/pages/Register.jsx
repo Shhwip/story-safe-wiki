17478a26a7b4582f9a2dac06dd369865951b2f46
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-10-10 01:19:12 -0600

    Routes Added for Header Navigation

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Register.css';
+import Header from "../Components/Header.jsx";
+
+function Register() {
+
+    return(
+        <div>
+            <Header />
+            <h1>Register Page</h1>
+        </div>
+    );
+}
+
+export default Register;
