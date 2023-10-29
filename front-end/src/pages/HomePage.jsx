b3f625f125655485e532e484c5d60eb918fe3d55
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-10-28 18:05:24 -0600

    Search Update 2.0
    Global Search fully working.
    Search Page Search Bar is non-functional

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -5,24 +5,12 @@ import axios from "axios";
 import Header from "../components/Header.jsx";
 import { useNavigate } from "react-router-dom";
 
-// Copying Josh's hello world
 function HomePage() {
-  const [displayMessage, setDisplayMessage] = useState(null);
-  let [loggedInUser, setLoggedInUser] = useState([]);
-  const navigate = useNavigate();
 
-  function handleToParserClick() {
-    navigate("/parse/Lung");
-  }
 
   return (
     <div>
       <Header />
-      <div className="other">
-        <button className="parser-button" onClick={handleToParserClick}>
-          To Parser
-        </button>
-      </div>
     </div>
   );
 }
--- front-end/src/pages/SearchPage.jsx
+++ front-end/src/pages/SearchPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from "react";
 import Header from "../components/Header.jsx";
+import "./SearchPage.css";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import FandomCommunityHeader from "../components/FandomCommunityHeader.jsx";
@@ -31,6 +32,16 @@ function SearchPage() {
             });
     }, [query]);
 
+    function truncateText(text, wordCount) {
+        const words = text.split(' ');
+        if (words.length <= wordCount) {
+            return text;
+        }
+        const truncatedText = words.slice(0, wordCount).join(' ');
+        return `${truncatedText}...`;
+    }
+
+
     return (
         <div>
             <div className="global-navigation">
@@ -46,26 +57,73 @@ function SearchPage() {
                             <div className="page-header">
                                 <div className="page-header__title-wrapper">
                                     <h1 className="page-header__title">
-                                        Search Page
+                                        Search Results
                                     </h1>
                                 </div>
                             </div>
-                            <div>
-                                {isLoading ? (
-                                    <p>Loading...</p>
-                                ) : (
-                                    <div>
-                                        {searchResults.length === 0 ? (
-                                            <p>No search results found</p>
-                                        ) : (
-                                            <ul>
-                                                {searchResults.map((result) => (
-                                                    <li key={result._id.toString()}>{result.title}</li>
-                                                ))}
-                                            </ul>
-                                        )}
-                                    </div>
-                                )}
+                            <div className="page-content">
+                                <div className="mw-body-content">
+                                    <section className="unified-search">
+                                        <form className="unified-search__form">
+                                            <div className="unified-search__input">
+                                                <div className="unified-search__input__wrapper__inner">
+                                                    <div className="unified-search__input__wrapper">
+                                                        <input className="unified-search__input__query" placeholder={query}/>
+                                                    </div>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 512 512"
+                                                        fill="#fff"
+                                                        className="search-result-button"
+                                                        //ref={selectRef}
+                                                        //onClick={handleHeaderSearchClick}
+                                                    >
+                                                        <path d="M256 64C150.13 64 64 150.13 64 256s86.13 192 192 192 192-86.13 192-192S361.87 64 256 64zm91.31 283.31a16 16 0 01-22.62 0l-42.84-42.83a88.08 88.08 0 1122.63-22.63l42.83 42.84a16 16 0 010 22.62z" />
+                                                        <circle cx="232" cy="232" r="56" />
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                        </form>
+
+                                        <div className="unified-search__layout">
+                                            <div className="unified-search__layout__main">
+                                                <p className="unified-search__results__count">
+                                                    About {searchResults.length} result
+                                                    {searchResults.length !== 1 ? 's' : ''} for{' '}
+                                                    <strong>"{query}"</strong>
+                                                </p>
+
+                                                {isLoading ? (
+                                                    <p>Loading...</p>
+                                                ) : (
+                                                    <div>
+                                                        {searchResults.length === 0 ? (
+                                                            <p></p>
+                                                        ) : (
+                                                            <ul className="unified-search__results">
+                                                                {searchResults.map((result) => (
+                                                                    <li className="unified-search__result" key={result._id.toString()}>
+                                                                        <article>
+                                                                            <h3 className="unified-search__result__header">
+                                                                                <a className="unified-search__result__title"
+                                                                                   href={`/parse/${encodeURIComponent(result.title)}`}>
+                                                                                    {result.title}
+                                                                                </a>
+                                                                            </h3>
+                                                                            <div className="unified-search__result__content">
+                                                                                {truncateText(result.text, 30)}
+                                                                            </div>
+                                                                        </article>
+                                                                    </li>
+                                                                ))}
+                                                            </ul>
+                                                        )}
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
                             </div>
                         </main>
                     </div>
