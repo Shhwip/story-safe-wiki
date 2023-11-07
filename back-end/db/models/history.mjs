2272a96b819237ae9d996abd3a19d111cdff503d
shhwip <shhhwip@gmail.com>
2023-11-07 16:08:00 -0700

    history model

---
+++ back-end/db/models/history.mjs
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const historySchema = new mongoose.Schema({
+    delta: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    previousID: {
+        type: String,
+        required: false,
+    },
+});
+
+export default mongoose.model('History', historySchema);
+
+
